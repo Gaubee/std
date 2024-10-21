fc9510e0ca323c2e5fbe81ca503e8d248093fb96
kezhaofeng <GaubeeBangeel@Gmail.com>
2024-10-21 21:54:48 +0800

    :recycle: 修复decorators导出的类型

--- util/src/decorators.ts
+++ util/src/decorators.ts
@@ -20,47 +20,46 @@ export type ClassGetterDecorator<T extends object, V> = (
 /**
  * 用于对 getter 的修饰
  */
-export const getter = Object.assign(<T extends object, V>(
+export const getter = <T extends object, V>(
     builder: ClassGetterDecorator<T, V>,
 ): ClassGetterDecorator<T, V> => {
     return builder;
-}, {
-    /**
-     * 用于在 getter 之前做一些动作
-     */
-    before: <T extends object, V>(before: Func<T, [], void>): ClassGetterDecorator<T, V> => {
-        return getter<T, V>((get) => {
-            return function () {
-                before.call(this);
-                return get.call(this);
-            };
-        });
-    },
-    /**
-     * 用于在 getter 之后做一些动作
-     */
-    after: <T extends object, V>(after: Func<T, [V], void>): ClassGetterDecorator<T, V> => {
-        return getter<T, V>((get) => {
-            return function (this: T) {
-                const result = get.call(this);
-                after.call(this, result);
-                return result;
-            };
-        });
-    },
-    /**
-     * 用于对 getter 做一个拦截
-     */
-    wrap: <T extends object, V>(
-        wrap: (this: T, get: () => V) => V,
-    ): ClassGetterDecorator<T, V> => {
-        return getter<T, V>((get) => {
-            return function (this: T) {
-                return wrap.call(this, () => get.call(this));
-            };
-        });
-    },
-});
+};
+/**
+ * 用于在 getter 之前做一些动作
+ */
+export const beforeGetter = <T extends object, V>(before: Func<T, [], void>): ClassGetterDecorator<T, V> => {
+    return getter<T, V>((get) => {
+        return function () {
+            before.call(this);
+            return get.call(this);
+        };
+    });
+};
+/**
+ * 用于在 getter 之后做一些动作
+ */
+export const afterGetter = <T extends object, V>(after: Func<T, [V], void>): ClassGetterDecorator<T, V> => {
+    return getter<T, V>((get) => {
+        return function (this: T) {
+            const result = get.call(this);
+            after.call(this, result);
+            return result;
+        };
+    });
+};
+/**
+ * 用于对 getter 做一个拦截
+ */
+export const wrapGetter = <T extends object, V>(
+    wrap: (this: T, get: () => V) => V,
+): ClassGetterDecorator<T, V> => {
+    return getter<T, V>((get) => {
+        return function (this: T) {
+            return wrap.call(this, () => get.call(this));
+        };
+    });
+};
 
 export type ClassSetterDecorator<T, V> = (
     target: (this: T, value: V) => void,
@@ -69,46 +68,45 @@ export type ClassSetterDecorator<T, V> = (
 /**
  * 用于对 setter 的修饰
  */
-export const setter = Object.assign(<T = unknown, V = unknown>(
+export const setter = <T = unknown, V = unknown>(
     builder: ClassSetterDecorator<T, V>,
 ): ClassSetterDecorator<T, V> => {
     return builder;
-}, {
-    /**
-     * 用于在 setter 之前做一些动作
-     */
-    before: <T extends object, V>(before: Func<T, [V], void>): ClassSetterDecorator<T, V> => {
-        return setter<T, V>((set) =>
-            function (v) {
-                before.call(this, v);
-                set.call(this, v);
-            }
-        );
-    },
-    /**
-     * 用于在 setter 之后做一些动作
-     */
-    after: <T extends object, V>(after: Func<T, [V], void>): ClassSetterDecorator<T, V> => {
-        return setter<T, V>((set) =>
-            function (v) {
-                set.call(this, v);
-                after.call(this, v);
-            }
-        );
-    },
-    /**
-     * 用于对 setter 做一个拦截
-     */
-    wrap: <T extends object, V>(
-        wrap: (this: T, value: V, set: Func<T, [V], void>) => void,
-    ): ClassSetterDecorator<T, V> => {
-        return setter<T, V>((set) =>
-            function (v) {
-                wrap.call(this, v, set);
-            }
-        );
-    },
-});
+};
+/**
+ * 用于在 setter 之前做一些动作
+ */
+export const beforeSetter = <T extends object, V>(before: Func<T, [V], void>): ClassSetterDecorator<T, V> => {
+    return setter<T, V>((set) =>
+        function (v) {
+            before.call(this, v);
+            set.call(this, v);
+        }
+    );
+};
+/**
+ * 用于在 setter 之后做一些动作
+ */
+export const afterSetter = <T extends object, V>(after: Func<T, [V], void>): ClassSetterDecorator<T, V> => {
+    return setter<T, V>((set) =>
+        function (v) {
+            set.call(this, v);
+            after.call(this, v);
+        }
+    );
+};
+/**
+ * 用于对 setter 做一个拦截
+ */
+export const wrapSetter = <T extends object, V>(
+    wrap: (this: T, value: V, set: Func<T, [V], void>) => void,
+): ClassSetterDecorator<T, V> => {
+    return setter<T, V>((set) =>
+        function (v) {
+            wrap.call(this, v, set);
+        }
+    );
+};
 
 export type ClassFieldDecorator<T extends object, V> = (
     target: undefined,
@@ -132,21 +130,21 @@ export type ClassMethodDecorator<T, M extends Method<T>> = (
  * @param builder
  * @returns
  */
-export const method = Object.assign(<T extends object, M extends Method<T>>(
+export const method = <T extends object, M extends Method<T>>(
     builder: ClassMethodDecorator<T, M>,
 ): ClassMethodDecorator<T, M> => {
     return builder;
-}, {
-    bindThis: <T extends object, M extends Method<T>>(): ClassMethodDecorator<T, M> =>
-        method<T, M>((target, context) => {
-            if (context.private) {
-                throw new Error("private method no suport to bind this.");
-            }
-            context.addInitializer(function () {
-                Reflect.set(this, context.name, target.bind(this));
-            });
-        }),
-});
+};
+
+export const bindThis = <T extends object, M extends Method<T>>(): ClassMethodDecorator<T, M> =>
+    method<T, M>((target, context) => {
+        if (context.private) {
+            throw new Error("private method no suport to bind this.");
+        }
+        context.addInitializer(function () {
+            Reflect.set(this, context.name, target.bind(this));
+        });
+    });
 
 type Class = abstract new (...args: any) => any;
 export type ClassDecorator<C extends Class> = (
