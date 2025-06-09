646d28014e44344b584a98848de842f3de4a8add
Gaubee <GaubeeBangeel@Gmail.com>
2025-06-09 16:01:24 +0800

    🎵 [nodekit] 暴露出Spinner的类型定义

--- nodekit/src/tui.ts
+++ nodekit/src/tui.ts
@@ -1,4 +1,14 @@
 import * as prompts from "@inquirer/prompts";
 import * as ora from "ora";
 const spinner = Object.assign(ora.default, ora);
+export type Spinner = ReturnType<typeof spinner>;
+export namespace Spinner {
+  export type Spinner = ora.Spinner;
+  export type Color = ora.Color;
+  export type PrefixTextGenerator = ora.PrefixTextGenerator;
+  export type SuffixTextGenerator = ora.SuffixTextGenerator;
+  export type Options = ora.Options;
+  export type PersistOptions = ora.PersistOptions;
+  export type PromiseOptions<T> = ora.PromiseOptions<T>;
+}
 export {prompts, spinner};
